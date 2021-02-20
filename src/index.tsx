b75cb48e9b2defd2e7d1fbd3dd335c613a77cb3c
Ryan Lee <ryanlee7132@gmail.com>
2021-02-20 14:07:57 -0500

    Finish landing page content

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom'
 import { container } from "./state";
 import "./index.css";
+import landingpage from './assets/landingpage.gif'
 
 ReactDOM.render(
     <React.StrictMode>
@@ -10,13 +11,16 @@ ReactDOM.render(
 
             <h3>BrickChain Voting</h3>
             <text>A block chain based transparent yet anonymous voting system</text>
-            <text>1. Login using your voter ID </text>
-            <text>2. Do Facial Authentication</text>
-            <text>3. Rank Your Vote</text>
-            <text>4. Submit Vote</text>
-            <text>5. See Current Results</text>
-            <text>6. See your Vote Block in the Chain</text>
 
+            <img src={landingpage}/>
+            <ul> 
+            <li>1. Login using your voter ID </li>
+            <li>2. Do Facial Authentication</li>
+            <li>3. Rank Your Vote</li>
+            <li>4. Submit Vote</li>
+            <li>5. See Current Results</li>
+            <li>6. See your Vote Block in the Chain</li>
+            </ul>
             <img src="https://cdn.discordapp.com/attachments/812517678140227614/812756666331299910/image.png"alt="Logo" />
 
         </container.Provider>
