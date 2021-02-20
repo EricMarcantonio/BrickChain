2206fa953ffae6270a18bdfa563cff9fdacebdeb
Eric Marcantonio <eric11marcantonio@gmail.com>
2021-02-20 17:32:44 -0500

    added temp backend communication

--- src/backend.ts
+++ src/backend.ts
@@ -1,8 +1,19 @@
-import a from 'axios'
-
+import a from "axios";
 
 const axios = a.create({
-    baseURL: "https://us-central1-aiot-fit-xlab.cloudfunctions.net/brickchainfunctions"
-})
+    baseURL:
+        "https://us-central1-aiot-fit-xlab.cloudfunctions.net/brickchainfunctions",
+});
 
+export const SendFaceDesc = (desc: Float32Array, userid: string) => {
+    return axios.post("/storeuserface", {
+        userid,
+        desc,
+    });
+};
 
+export const GetFaceDesc = (userid: string) => {
+    return axios.post("/getuserface", {
+        userid,
+    });
+};
