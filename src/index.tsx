94cc910e03b0d4b4bceab4efe0fc3a28081b1d9b
sehmim2 <sehmim2010@hotmail.com>
2021-02-20 15:26:09 -0500

    adding routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,77 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from 'react-dom'
 import { container } from "./state";
 
+// ROutes
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
+
+// Pages
+import LandingPage from "./pages/LandingPage"
+import FinalPage from "./pages/FinalPage"
+import VideoAuthPage from "./pages/VideoAuthPage"
+import VotePage from "./pages/VotePage"
+import VoterloginPage from "./pages/VoterloginPage"
+
 import "./index.css";
+
+
+const AuthRoute = () => (
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <LandingPage />
+                </Route>
+                <Route path="/voter-login">
+                    <VoterloginPage />
+                </Route>
+            </Switch>
+        </Router>
+    )
+
+const AppRoute = () => 
+    (<Router>
+        <Switch>
+            <Route path="/">
+                <LandingPage />
+            </Route>
+            <Route path="/voter-login">
+                    <VoterloginPage />
+            </Route>
+            <Route path="/video-auth">
+                <VideoAuthPage />
+            </Route>
+            <Route path="/vote">
+                <VotePage />
+            </Route>
+            <Route path="/vote">
+                <FinalPage />
+            </Route>
+        </Switch>
+    </Router>)
+
+const App = () => {
+
+    // This state is suppose to come from Unstated States in container
+    const [ isLoggedIn, setIsLoggedIn] = useState(true)
+
+    return (
+        isLoggedIn ? <AppRoute /> : <AuthRoute />
+    )
+
+}
 
 
 ReactDOM.render(
     <React.StrictMode>
         <container.Provider>
-
+            <App />
         </container.Provider>
     </React.StrictMode>,
     document.getElementById("root")
