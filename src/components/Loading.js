835481799198b73336a02346705e4c9d00e7faad
sehmim2 <sehmim2010@hotmail.com>
2021-02-20 20:24:59 -0500

    adding shit

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,14 +2,39 @@ import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import BLOCKS  from "../assets/brick.gif"
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
-      '& > * + *': {
-        marginLeft: theme.spacing(2),
-      },
+        backgroundColor: "rgba(128, 128, 128, 0.205)",
+        textAlight: "center",
+        height: "100vh",
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItem: "center"
     },
+    header: {
+        fontSize: '40px',
+        fontWeight: "500",
+        textAlign: "center",
+        marginBottom: "30px",
+        backgroundColor: "white",
+        padding: "50px"
+    },
+    top: {
+        width: "100%",
+    },
+    div: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    img: {
+        width: "600px",
+        borderRadius: "15px" 
+    }
   }),
 );
 
@@ -18,7 +43,14 @@ export default function CircularIndeterminate() {
 
   return (
     <div className={classes.root}>
-      <CircularProgress />
+        <p className={classes.top}></p>
+        <div>
+            <h1 className={classes.header}>Validating Chain and adding your vote to Block</h1>
+        </div>
+        <hr></hr>
+        <div className={classes.div}>
+            <img className={classes.img} src={BLOCKS}></img>
+        </div>
     </div>
   );
 }
