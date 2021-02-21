a900c252d4aef2691cfb07f8fbf0f78d77e8c090
sehmim2 <sehmim2010@hotmail.com>
2021-02-20 20:03:55 -0500

    adding vote page

--- src/state.ts
+++ src/state.ts
@@ -6,10 +6,31 @@ export const container = createContainer(() => {
     const [isAuthenticated, setIsAutheticated]  = useState(true)   
     const [webCamPhoto, setWebCamPhoto] = useState(null);
 
+
+    // Voting States
+    const [firstOption, setFirstOption] = useState(null);
+    const [secondOption, setSecondOption] = useState(null);
+    const [thridOption, setThirdOption] = useState(null);
+    const [forthOption, setFourthOption] = useState(null);
+    const [fifthOption, setFifthOption] = useState(null);
+
+    // View State
+    const [ sumbitVote, setSumbitVote ] = useState(false);
+
     return {
         isAuthenticated,
         setIsAutheticated,
-        
+
+        // voting
+        firstOption, setFirstOption,
+        secondOption, setSecondOption,
+        thridOption, setThirdOption,
+        forthOption, setFourthOption,
+        fifthOption, setFifthOption,
+
+        // View States
+        sumbitVote, setSumbitVote,
+
         webCamPhoto,
         setWebCamPhoto
     };
