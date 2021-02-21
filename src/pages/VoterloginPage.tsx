58170ccb2d04171c7fd51d11e959646411f21b34
sehmim2 <sehmim2010@hotmail.com>
2021-02-20 21:53:19 -0500

    adding dummy login

--- src/pages/VoterloginPage.tsx
+++ src/pages/VoterloginPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FaceID } from '../FaceID';
 
@@ -6,9 +6,39 @@ import loginpage from '../assets/loginpage.png';
 
 import Divider from '@material-ui/core/Divider';
 
+import { useHistory } from "react-router-dom";
+
+import CircularIndeterminate from '../components/Loading'
+
+
 function VoterloginPage () {
+    let history = useHistory();
+
+    const [ email, setEmail ] = useState()
+    const [ password, setPassword ] = useState()
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(false)
+
+ 
+    const doLogIn = () => {
+
+        if (email === "test@test.com" && password === "bricks") {
+            console.log(email, password)
+            setLoading(true)
+    
+            setTimeout(() => {
+                setLoading(false)
+                history.push("/vote")
+            }, 800);
+        } else {
+            setError(true)
+        }
+    }
+
     return (
         <>
+            {
+                loading ? <CircularIndeterminate /> :
             <div
                 style={{
                     alignItems: 'center',
@@ -16,16 +46,18 @@ function VoterloginPage () {
                     flexDirection: 'column',
                     marginTop: 100
                 }}
-            >
+            >   
                 <h3 style={{ fontSize: 24, fontWeight: 'bold' }}>
                     Voter Verification
                 </h3>
+                { error ? <h1>Login Failed Try Again</h1> : null} 
+
                 <Divider />
 
                 <img src={loginpage} style={{ width: 500 }} alt='Logo' />
 
                 <form
-                    onSubmit={() => {}}
+                    onSubmit={doLogIn}
                     style={{ flexDirection: 'column', display: 'flex' }}
                 >
                     <label style={{ fontSize: 20, marginTop: 20 }}>Email</label>
@@ -34,10 +66,12 @@ function VoterloginPage () {
                         style={{
                             borderWidth: 2,
                             borderColor: '#777',
-                            width: 300
+                            width: 300,
+                            paddingLeft: 10,
+
                         }}
                         type='text'
-                        onChange={() => {}}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
 
                     <label style={{ fontSize: 20, marginTop: 20 }}>
@@ -47,14 +81,16 @@ function VoterloginPage () {
                         style={{
                             borderWidth: 2,
                             borderColor: '#777',
-                            width: 300
+                            width: 300,
+                            paddingLeft: 10,
                         }}
-                        type='text'
-                        onChange={() => {}}
+                        type='password'
+                        onChange={(e) => setPassword(e.target.value)}
                     />
 
                     <input
                         type='submit'
+                        onClick={doLogIn}  
                         style={{
                             backgroundColor: '#000',
                             color: '#fff',
@@ -62,11 +98,36 @@ function VoterloginPage () {
                             marginTop: 60,
                             padding: 10,
                             borderRadius: 5,
-                            margin: 10
+                            margin: 10,
+                            cursor: "pointer"
                         }}
                     />
                 </form>
+
+                <button
+                    onClick={() => history.push("user-onboarding")}
+                    type='button'
+                    style={{
+                        backgroundColor: '#000',
+                        color: '#fff',
+                        fontSize: 15,
+                        marginTop: 60,
+                        padding: 10,
+                        borderRadius: 5,
+                        margin: 10,
+                    }}
+                >Do On Boarding</button>
+
+                <div className="text-center">
+                    <h1><b>Test User</b></h1>
+                    <ul>
+                        <li> email: test@test.com </li>
+                        <li> Password: bricks </li>
+                    </ul>
+                </div>
+
             </div>
+            }
         </>
     );
 }
