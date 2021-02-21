87d9d9214e0706e308bb5111f672800bfb7bbcdf
Ryan Lee <ryanlee7132@gmail.com>
2021-02-20 20:11:14 -0500

    Finish landing page

--- src/pages/VoterloginPage.tsx
+++ src/pages/VoterloginPage.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import {FaceID} from '../FaceID'
+
+import loginpage from '../assets/loginpage.png'
+
 function VoterloginPage() {
     return (
-        <div>
-            Voter Login Page
-        </div>
+        <>
+            <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column'}}>  
+            <h3>Voter Verification</h3>
+
+            <img src="https://cdn.discordapp.com/attachments/812517678140227614/812756666331299910/image.png"alt="Logo" />
+            
+            <form>
+                <label>Name:
+                    <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            </div>
+        </>
     )
 }
 
